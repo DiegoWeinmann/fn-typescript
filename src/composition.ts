7cab6d9236c55ad11a4a01fd75302610658e163c
Diego Weinmann <diego.weinmann@hotmail.com>
2020-04-11 16:12:58 -0300

    refactor: composition

--- src/composition.ts
+++ src/composition.ts
@@ -5,21 +5,13 @@ const testString = 'Diego    ';
 
 const compose = <T>(f: (x: T) => T, g: (x: T) => T) => (x: T) => f(g(x));
 const composeMany = <T>(...functions: Array<(arg: T) => T>) => (arg: any) =>
-<<<<<<< HEAD
 	functions.reduce(
 		(prev, currentFunction) => currentFunction(prev),
 		arg
 	);
-=======
-	functions.reduce((prev, current) => current(prev), arg);
->>>>>>> 74a7c69e96f244aeee7902f140bc49806895b45d
 
 const trimAndCapitalize = compose(trim, capitalize);
 
 console.log(trimAndCapitalize(testString));
 
-<<<<<<< HEAD
 console.log(composeMany(trim, capitalize)(testString));
-=======
-console.log(composeMany(trim, capitalize)(testString))
->>>>>>> 74a7c69e96f244aeee7902f140bc49806895b45d
